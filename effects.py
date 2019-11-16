cf6cfd84644c30359d3cc9eac49e4ead1af03448
Dmitry Egorov <mail@degorov.com>
2019-11-17 01:11:06 +0300

    Add full hue loop effect

--- effects.py
+++ effects.py
@@ -3,11 +3,14 @@ import uos
 import glyph
 
 
+# ============================================================================ #
+
 def all_random():
     for x in range(led.LED_WIDTH):
         for y in range(led.LED_HEIGHT):
             led.led_matrix[x][y] = (ord(uos.urandom(1)), ord(uos.urandom(1)), int(ord(uos.urandom(1)) / 2))
 
+# ============================================================================ #
 
 loop_numbers_counter = 0
 loop_numbers_number = 0
@@ -27,3 +30,19 @@ def loop_numbers():
         loop_numbers_counter = 0
     else:
         loop_numbers_counter += 1
+
+# ============================================================================ #
+
+all_hue_loop_hue = 0
+
+def all_hue_loop():
+    global all_hue_loop_hue
+
+    for x in range(led.LED_WIDTH):
+        for y in range(led.LED_HEIGHT):
+            led.led_matrix[x][y] = (all_hue_loop_hue, 255, 192)
+
+    if all_hue_loop_hue == 256:
+        all_hue_loop_hue = 0
+    else:
+        all_hue_loop_hue += 1
